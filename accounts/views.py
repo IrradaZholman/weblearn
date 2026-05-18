6bb7afbdcccd9819389c4835d431b1cc42559f07
IrradaZholman <164073065+IrradaZholman@users.noreply.github.com>
2026-05-18 18:20:25 +0500

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.models import User
 
 
 class CustomLoginView(LoginView):

abc0394e354ba05fb38091c3922ee2f28679e79e
IrradaZholman <164073065+IrradaZholman@users.noreply.github.com>
2026-05-19 00:10:23 +0500

    Update settings.py

--- weblearn/settings.py
+++ weblearn/settings.py
@@ -169,6 +169,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'main:home'
 
-LOGOUT_REDIRECT_URL = 'main:home'
-
+LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = 'accounts:login'

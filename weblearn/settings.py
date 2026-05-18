3da008eef620cddfc56eba487da10fb52e7280b5
IrradaZholman <164073065+IrradaZholman@users.noreply.github.com>
2026-05-18 23:54:43 +0500

    Update settings.py

--- weblearn/settings.py
+++ weblearn/settings.py
@@ -101,11 +101,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'weblearn.wsgi.application'
 
 import dj_database_url
-import os
-
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL")
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
     )
 }
 

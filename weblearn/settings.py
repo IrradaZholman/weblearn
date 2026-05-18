c6a0c03bef7bebba8b374a86079cc960501ed924
IrradaZholman <164073065+IrradaZholman@users.noreply.github.com>
2026-05-18 23:43:10 +0500

    Refactor database settings to use dj_database_url
    Updated database configuration to use dj_database_url for DATABASE_URL.

--- weblearn/settings.py
+++ weblearn/settings.py
@@ -100,11 +100,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'weblearn.wsgi.application'
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [

7f065813964486eea03c81e6826d221ee3deb24a
IrradaZholman <164073065+IrradaZholman@users.noreply.github.com>
2026-05-19 00:53:17 +0500

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -8,22 +8,34 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 
+def create_admin(request):
+
+    if not User.objects.filter(username='admin').exists():
+
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@gmail.com',
+            password='admin12345'
+        )
+
+        return HttpResponse("ADMIN CREATED")
+
+    return HttpResponse("ADMIN ALREADY EXISTS")
+
+
 class CustomLoginView(LoginView):
 
     template_name = 'accounts/login.html'
 
     redirect_authenticated_user = True
 
-    # КУДА ПЕРЕНАПРАВЛЯТЬ ПОСЛЕ ВХОДА
     def get_success_url(self):
 
         user = self.request.user
 
-        # ЕСЛИ АДМИН
         if user.is_superuser:
             return '/admin/'
 
-        # ЕСЛИ ОБЫЧНЫЙ ПОЛЬЗОВАТЕЛЬ
         return '/'
 
 
@@ -42,7 +54,6 @@ def register(request):
 
             login(request, user)
 
-            # ПОСЛЕ РЕГИСТРАЦИИ
             if user.is_superuser:
                 return redirect('/admin/')
 
@@ -56,18 +67,3 @@ def register(request):
         'accounts/register.html',
         {'form': form}
     )
-
-
-def create_admin(request):
-
-    if not User.objects.filter(username='admin').exists():
-
-        User.objects.create_superuser(
-            username='admin',
-            email='admin@gmail.com',
-            password='admin12345'
-        )
-
-        return HttpResponse("Админ создан")
-
-    return HttpResponse("Админ уже существует")
